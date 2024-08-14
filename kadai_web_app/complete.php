0b2a1ac55f9b894dc78e42b3f07dfd2f6c8a80bf
k-nosung <tiroruporo.love@gmail.com>
2024-08-14 11:40:37 +0900

    kadai_web_app　アップロード

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>完了画面</title>
+</head>
+<body>
+
+<h2>登録が完了しました</h2>
+
+<p>社員情報がデータベースに保存されました。</p>
+
+<form action="form.php" method="get">
+    <input type="submit" value="戻る">
+</form>
+
+</body>
+</html>
